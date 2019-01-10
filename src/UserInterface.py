df4ba4bae5d9f9dff93feaac7f96db773a6efb70
Alireza <alirezafa@rocketmail.com>
2019-01-10 23:26:06 +0330

    Fixed TA's mistake!

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -6,11 +6,11 @@ class UserInterface(threading.Thread):
     buffer = []
 
 
-def run(self):
-    """
-    Which the user or client sees and works with.
-    This method runs every time to see whether there are new messages or not.
-    """
-    while True:
-        message = input("Write your command:\n")
-        self.buffer.append(message)
+    def run(self):
+        """
+        Which the user or client sees and works with.
+        This method runs every time to see whether there are new messages or not.
+        """
+        while True:
+            message = input("Write your command:\n")
+            self.buffer.append(message)
